6a2df7fd3c9092e452ce827bb15aaeed7b19f14b
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-07-11 12:54:09 +0200

    add explanations for dynamic and static linking

---
+++ linking-libraries/staticLinking.cpp
@@ -0,0 +1,9 @@
+/*
+statick linking means that all the code from the librries you use is 
+copied directly into your executable. 
+This results in a larger .exe file but i can run in any system 
+without worrying about dependencies.
+*/ 
+
+
+// Find a tutorial for statick linking in VScode.

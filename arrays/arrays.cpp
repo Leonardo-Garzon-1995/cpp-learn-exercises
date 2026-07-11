a872882ee0b6cfa80faf21786f2a06acf0ef7765
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2026-07-11 12:52:30 +0200

    add explanation for static arrays (int arr[5], and std::array<int, 5>)

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <array>
 
-// RAW ARRAYS
+// RAW/STATIC ARRAYS
 
 int arr[5]; //inizialization with length 5
 
@@ -15,4 +15,14 @@ std::array<int, 5> secondArr {};
 
 void setValueSecondArr(const int& v, const int& index) {
     secondArr[index] = v;
+}
+
+int main () {
+    setValueArr(10, 0);
+    setValueSecondArr(20, 1);
+
+    std::cout << "arr[0]: " << arr[0] << '\n';
+    std::cout << "secondArr[0]: " << secondArr[0] << '\n';
+    std::cout << "secondArr[1]: " << secondArr[1] << '\n';
+    return 0;
 }
